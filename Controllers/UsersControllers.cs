1654067a7752f9fb156cd8ded6cb94b8c07f5f44
staylazy <staylazy@yandex.ru>
2023-03-22 21:24:26 +0700

    Added docker support
    Docker is essential for our web project as it allows us to create a consistent and reproducible environment for our application to run in. With Docker, we can package our application along with all its dependencies into a single container, making it easy to deploy and scale our app across multiple environments. Additionally, Docker's isolation and portability features make it easier to manage and maintain our application, saving us time and effort in the long run.

---
+++ Controllers/UsersControllers.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ckmz.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersControllers : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = new[]
+            {
+                new { Name = "Oleg" },
+                new { Name = "Ivan" }
+            };
+
+            return Ok(users);
+        }
+    }
+}
